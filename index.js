9a4a502bd35dec8662a5ad785e3fb0c29caffb31
Md. Safwan <sayedmdsafwan@gmail.com>
2022-05-11 23:02:52 +0600

    mongodb server created and connected to the client side

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 4000;
+require("dotenv").config();
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhygh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+async function run() {
+    try {
+        await client.connect();
+
+        const serviceCollection = client
+            .db("doctors_portal")
+            .collection("services");
+
+        // get all data
+        app.get("/service", async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
